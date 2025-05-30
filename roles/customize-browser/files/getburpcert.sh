d2f13c572205708d85bd37eb22bccc1e516410c8
spencerja <spencerjaquet@gmail.com>
2025-05-30 01:37:07 -0500

    change include_tasks syntax, switch to netexec

--- roles/customize-browser/files/getburpcert.sh
+++ roles/customize-browser/files/getburpcert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
 /bin/bash -c "timeout 45 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
 sleep 30
 curl http://localhost:8080/cert -o /tmp/cacert.der
